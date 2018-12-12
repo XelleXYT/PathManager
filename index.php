e2620e0211a96403c40531c86031555785dba253
Alejandro <alejandro@xellex.es>
2018-12-12 06:26:21 +0100

    Suprimidos jQuerry y Boostrap script import.

--- index.php
+++ index.php
@@ -61,9 +61,6 @@ if (session_status() == PHP_SESSION_NONE) {
             </div>
         </div>
     </body>
-    
-    <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
-    <script src="js/bootstrap.min.js" type="text/javascript"></script>
 
     <script>
         var sesion;
